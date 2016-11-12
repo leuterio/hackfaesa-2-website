8c614a58a07469fc7b328641c16c47d234668bc8
Gladison <gladison.sonik@gmail.com>
2016-11-12 20:11:40 -0200

    Adiçao de angular a index

--- js/home.controller.js
+++ js/home.controller.js
@@ -9,17 +9,40 @@
     function HomeController($scope, HomeService) {
         var vm = this;
         vm.itemBusca = {};
+        vm.userLogin = {};
+        vm.userCadastro = {};
         vm.items = [];
+        vm.modalCadastro = vm.modalLogin = vm.modalAjuda = false;
 
         activate();
 
         ////////////////
+        function cadastrar(){
+            vm.modalCadastro = true;
+        }
+        function fecharCadastro(){
+            vm.modalCadastro = false;
+        }
+        function efetuarCadastro(){
+            debugger;
+            console.log(vm.userCadastro);
+            //Cadastrar usuario;
+            fecharCadastro();
+        }
+
         function acessar() {
             vm.modalLogin = true;
         }
         function fecharLogin() {
             vm.modalLogin = false;
         }
+        function efetuarLogin(){
+            debugger;
+            console.log(vm.userLogin);
+            //Logar   
+            fecharLogin();
+        }
+
         function ajuda() {
             vm.modalAjuda = true;
         }
@@ -29,32 +52,60 @@
 
         function buscar() {
             vm.items = HomeService.getAnumcios();
+            let arrayAux = [];
             function filtraItens(item, index){
                 if(vm.itemBusca.onde){
-
+                    if(item.endereco && item.endereco.toLowerCase().indexOf(vm.itemBusca.onde.toLowerCase()) !== -1){
+                        return item;
+                    }
                 }
                 if(vm.itemBusca.tipo){
-
+                    if(item.tipoAnuncio && item.tipoAnuncio == vm.itemBusca.tipo){
+                        return item;
+                    }
                 }
-                if(vm.itemBusca.inicio){
-
-                }
-                if(vm.itemBusca.fim){
-
+                if(vm.itemBusca.data){
+                    if(
+                        (item.dateInicio && item.dateInicio >= vm.itemBusca.inicio && item.dataExpira && item.dataExpira <= vm.itemBusca.fim) 
+                            || (item.dataExpira && item.dataExpira <= vm.itemBusca.fim) ){
+                        return item;
+                    }
                 }
             }
-            vm.items = vm.items.map(filtraItens);
+            arrayAux = vm.items.reduce(filtraItens, arrayAux);
+            debugger;
+            vm.items = arrayAux;
         }
 
         function activate() { 
             vm.titulo = "OrtBook"
+            
             vm.acessar = acessar;
             vm.fecharLogin = fecharLogin;
+            vm.efetuarLogin = efetuarLogin;
+
             vm.ajuda = ajuda;
             vm.fecharAjuda = fecharAjuda;
             vm.buscar = buscar;
+            
+            vm.cadastrar = cadastrar;
+            vm.fecharCadastro = fecharCadastro;
+            vm.efetuarCadastro = efetuarCadastro;
+
+
 
         }
+
+        $scope.safeApply = function(fn) {
+            var phase = this.$root.$$phase;
+            if(phase == '$apply' || phase == '$digest') {
+                if(fn && (typeof(fn) === 'function')) {
+                fn();
+                }
+            } else {
+                this.$apply(fn);
+            }
+        };
 
     }
 })(angular);
